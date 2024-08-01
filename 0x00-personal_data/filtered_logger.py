29e21a21f2381e593a60272e4b08199b79bed8ce
1mansseh23 <cletavivan@gmail.com>
2024-08-01 10:18:05 -0700

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,19 +7,24 @@ import re
 from typing import List, Tuple
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-        """
-        Obfuscates sensitive fields in a log message.
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str
+        ) -> str:
+    """
+    Obfuscates sensitive fields in a log message.
 
-        Args:
-            fields (List[str]): A list of field names to be obfuscated.
-            redaction (str): The string to use for obfuscation.
-            message (str): The log message to be processed.
-            separator (str): The character used to separate fields in the log message.
+    Args:
+    fields (List[str]): A list of field names to be obfuscated.
+    redaction (str): The string to use for obfuscation.
+    message (str): The log message to be processed.
+    separator (str): The character used to separate fields in the log message.
 
-        Returns:
-                str: The log message with the sensitive fields obfuscated.
-        """
+    Returns:
+        str: The log message with the sensitive fields obfuscated.
+    """
 
-        pattern = r'({})=.*?{}'.format('|'.join(fields), separator)
-        return re.sub(pattern, r'\1={}{}'.format(redaction, separator), message)
+    pattern = r'({})=.*?{}'.format('|'.join(fields), separator)
+    return re.sub(pattern, r'\1={}{}'.format(redaction, separator), message)
