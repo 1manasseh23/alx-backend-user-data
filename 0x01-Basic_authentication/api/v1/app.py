aaf8db801ebcaf7fae1340777c00ae0afe8f1050
1mansseh23 <cletavivan@gmail.com>
2024-08-07 14:26:59 -0700

    ff

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,7 @@ from api.v1.views import index
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
 
 @app.errorhandler(403)
