1a40d45cf6ed37737952c801f249fd4a9b831e92
1manasseh23 <cletavivan@gmail.com>
2024-08-07 22:24:55 +0000

    first

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -28,7 +28,7 @@ def view_one_user(user_id: str = None) -> str:
         if request.current_user is None:
             abort(404)
         return jsonify(request.current_user.to_json())
-    
+
     user = User.get(user_id)
     if user is None:
         abort(404)
@@ -117,4 +117,3 @@ def get_user_me() -> str:
     if request.current_user is None:
         abort(404)
     return jsonify(request.current_user.to_json())
-
