d3ef0c1494b07e35a03b027ed11318fdbc566561
1manasseh23 <136644538+1manasseh23@users.noreply.github.com>
2024-08-07 17:48:11 -0700

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -75,6 +75,12 @@ def forbidden(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
+@app.route('/api/v1/status', methods=['GET'])
+def status():
+    """ Returns the status of the API """
+    return jsonify({"status": "OK"})
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
