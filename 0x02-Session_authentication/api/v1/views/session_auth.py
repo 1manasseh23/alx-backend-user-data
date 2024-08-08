d0f4c842f405bdff14eb247b4624891ee2d71f14
1manasseh23 <136644538+1manasseh23@users.noreply.github.com>
2024-08-07 18:30:51 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -34,6 +34,35 @@ def auth_session():
     return jsonify({"error": "wrong password"}), 401
 
 
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def auth_session_login():
+    """
+    Handle the session authentication login
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({'email': email})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = user[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    session_name = getenv('SESSION_NAME')
+    response.set_cookie(session_name, session_id)
+    
+    return response
+
+
 @app_views.route('/auth_session/logout', methods=['DELETE'],
                  strict_slashes=False)
 def handle_logout():
