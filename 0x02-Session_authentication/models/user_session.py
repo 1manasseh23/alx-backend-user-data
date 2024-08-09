1ced0f112ccd6bb1b30899eb2999a296fff473de
1manasseh23 <136644538+1manasseh23@users.noreply.github.com>
2024-08-09 04:00:53 -0700

    user_session.py

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
-"""Module for user session
 """
+Module for user session.
+
+This module defines the `UserSession` class, which represents a session
+associated with a user. It inherits from the `Base` class.
+"""
+
 from models.base import Base
 
 
 class UserSession(Base):
-    """User session class.
+    """
+    User session class.
+
+    This class is used to create and manage user sessions, storing the
+    session ID and associated user ID.
     """
 
     def __init__(self, *args: list, **kwargs: dict):
-        """Initializes a User session instance.
+        """
+        Initializes a UserSession instance.
+
+        Args:
+            *args (list): Variable length argument list.
+            **kwargs (dict): Arbitrary keyword arguments, expected to contain
+            'user_id' and 'session_id'.
         """
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
