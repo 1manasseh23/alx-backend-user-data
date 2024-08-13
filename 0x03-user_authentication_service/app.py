e7117de2251277770ed476ffe744b24499f27e16
1manasseh23 <cletavivan@gmail.com>
2024-08-13 14:51:12 +0000

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort, make_response
 from auth import Auth
+import logging
+
+logging.disable(logging.WARNING)
 
 app = Flask(__name__)
 
@@ -29,6 +32,29 @@ def users():
     except ValueError:
         # If the user already exists, return an error message
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Login function to handle user sessions."""
+    # Extract form data
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # Validate credentials
+    if not AUTH.valid_login(email, password):
+        abort(401)  # Unauthorized if login fails
+
+    # Create a session and generate session ID
+    session_id = AUTH.create_session(email)
+
+    # Prepare the response
+    response = jsonify({"email": email, "message": "logged in"})
+
+    # Set the session_id in the response cookie
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
