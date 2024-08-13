351a1d246c1b382fe2ca28e9858fa77675107ebe
1manasseh23 <cletavivan@gmail.com>
2024-08-13 14:21:50 +0000

    Get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,9 +5,16 @@ string arguments and returns bytes
 """
 import bcrypt
 from db import DB
+from typing import Optional
 from user import User
 from bcrypt import hashpw, gensalt
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
+
+
+def _generate_uuid() -> str:
+    """Generate a new UUID and return its string representation."""
+    return str(uuid.uuid4())
 
 
 def _hash_password(password: str) -> bytes:
@@ -25,6 +32,28 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
+
+    def create_session(self, email: str) -> str:
+        """Creates a session and returns the session ID as a string.
+        Args:
+            email (str): Email of user to create session for.
+        Returns:
+            str: Session ID.
+        """
+        try:
+            # Find the user corresponding to the email
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            # Return None if no user is found with given email
+            return None
+        # If user is None, return None
+        if user is None:
+            return None
+        # Generate a new UUID and store it in the db as the user’s session_id
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        # Return the session ID.
+        return session_id
 
     def register_user(self, email: str, password: str) -> User:
         """Registers a new user with the provided email and password."""
